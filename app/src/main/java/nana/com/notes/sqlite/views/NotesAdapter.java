ad0aabffe6b2b8395c2fb05232c1ab031817493b
youngstruggle <febrian.nana@gmail.com>
2018-05-20 12:48:51 +0700

    Initial commit

---
+++ app/src/main/java/nana/com/notes/sqlite/views/NotesAdapter.java
@@ -0,0 +1,88 @@
+package nana.com.notes.sqlite.views;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import nana.com.notes.R;
+import nana.com.notes.sqlite.database.model.Note;
+
+/**
+ * Created by Nana Febriana on 20-May-18.
+ */
+
+public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.MyViewHolder> {
+    private Context context;
+    private List<Note> notesList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView note;
+        public TextView dot;
+        public TextView timestamp;
+
+        public MyViewHolder(View view) {
+            super(view);
+            note = (TextView) view.findViewById(R.id.note);
+            dot = (TextView) view.findViewById(R.id.dot);
+            timestamp = (TextView) view.findViewById(R.id.timestamp);
+        }
+    }
+
+    public NotesAdapter(Context context, List<Note> notesList) {
+        this.context = context;
+        this.notesList = notesList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.note_list_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Note note = notesList.get(position);
+
+        holder.note.setText(note.getNote());
+
+        // Displaying dot from HTML character code
+        holder.dot.setText(Html.fromHtml("&#8226;"));
+
+        // Formatting and displaying timestamp
+        holder.timestamp.setText(formatDate(note.getTimestamp()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notesList.size();
+    }
+
+    /**
+     * Formatting timestamp to `MMM d` format
+     * Input: 2018-02-21 00:15:42
+     * Output: Feb 21
+     */
+    private String formatDate(String dateStr) {
+        try {
+            SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date date = fmt.parse(dateStr);
+            SimpleDateFormat fmtOut = new SimpleDateFormat("MMM d");
+            return fmtOut.format(date);
+        } catch (ParseException e) {
+
+        }
+
+        return "";
+    }
+}
